f83c1084c5fcd04f1e7924d642f92a4c193aada0
Zhuoran Yu <gordonyuzr@gmail.com>
2018-03-08 19:39:16 -0500

    3.8
    3.8

--- src/main/java/q02_listMap/ListMap.java
+++ src/main/java/q02_listMap/ListMap.java
@@ -1,43 +1,52 @@
 package q02_listMap;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 
 public class ListMap implements ListMapInterface {
+    private Map<String, LinkedList<Integer>> map = new HashMap<>();
 
     @Override
     public void add(String key, Integer value) {
+        LinkedList<Integer> list = map.containsKey(key) ? map.get(key) : new LinkedList<>();
+        list.add(value);
+        map.put(key, list);
 
     }
 
     @Override
     public List<Integer> get(String key) {
-        return null;
+        return map.get(key);
     }
 
     @Override
     public void removeFirst(String key, Integer value) {
-
+        map.get(key).removeFirstOccurrence(value);
     }
 
     @Override
     public void removeLast(String key, Integer value) {
-
+        map.get(key).removeLastOccurrence(value);
     }
 
     @Override
     public void removeAll(String key, Integer value) {
+        while (map.get(key).remove(value)){
 
+        }
     }
 
     @Override
     public void remove(String key) {
-
+        map.remove(key);
     }
 
     @Override
     public boolean containsKey(String key) {
-        return false;
+        return map.containsKey(key);
     }
 
 }
